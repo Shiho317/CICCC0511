86e743ce6cc1c5dae2986fbe1b602832e5253244
Shiho Nagano <ex3jsb.geneg@gmail.com>
2022-05-12 10:08:54 -0700

    fixed

--- routes.js
+++ routes.js
@@ -9,57 +9,20 @@ router.get("/leave-note", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "views", "leave-note.html"));
 });
 
-router.get("/read-note", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "views", "read-note.html"));
-});
-
-router.post("/read-note", (req, res) => {
-  const noteLists = [];
+router.post("/leave-note", (req, res) => {
   const addedNotes = {
     user: req.body.user,
     title: req.body.title,
     note: req.body.note,
   };
+  const noteLists = req.noteLists;
   noteLists.push(addedNotes);
-  res.send(`
-  <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Guest Book</title>
-    <link rel="stylesheet" href="/css/style.css">
-  </head>
-  <body>
-    <header>
-      <h3>GUEST BOOK</h3>
-      <ul>
-        <a href="/">
-          <li>Home</li>
-        </a>
-        <a href="/read-note">
-          <li>Read Note</li>
-        </a>
-        <a href="/leave-note">
-          <li>Write Note</li>
-        </a>
-      </ul>
-    </header>
-    <main>
-      <h1>Our Notes</h1>
-      <div class="notes-wrapper">
-        ${noteLists.map((note) => {
-          return`<div class="note">
-            <h3>Title: ${note.title}</h3>
-            <p>Memo: ${note.note}</p>
-            <h4>From: ${note.user}</h4>
-          </div>`;
-        })}
-      </div>
-    </main>
-  </body>
-  </html>
-  `);
+  res.redirect('/read-note')
+});
+
+router.get("/read-note", (req, res) => {
+  const noteLists = req.noteLists;
+  res.render("read-note", { noteLists })
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,11 +4,21 @@ const bodyParser = require('body-parser');
 const routes = require('./routes');
 const path = require('path');
 
+app.set('view engine', 'ejs');
+app.set('views', 'public/views');
+
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, 'public')))
 
+const noteLists =[];
+
+app.use((req, res, next) => {
+  req.noteLists = noteLists;
+  next();
+})
+
 app.use(routes)
-app.use('/note', routes)
+
 
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, 'public', 'views', '404error.html'))
